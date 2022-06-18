46c5e40655182694c8b2bba4fa6ec13ff8cfac75
Max1miliano <maxipompas@hotmail.com>
2022-06-18 18:38:52 -0300

    agregando estilos al nav

--- ecommerce-semas/src/components/NavBar/navBar.jsx
+++ ecommerce-semas/src/components/NavBar/navBar.jsx
@@ -1,14 +1,13 @@
 import CartWidget from "../cartWidget/cartWidget";
 import {Link, NavLink} from 'react-router-dom'
+import '../NavBar/navBar.css'
 
 const NavBar = ({landing}) => {
     return <>
-        <header>
+        <header className="navBar">
             <nav>
                 <ul className="navItems">
-                    <Link to='/'>
-                        <h1>{landing}</h1>
-                    </Link>
+                    <Link to='/'><h1>{landing}</h1></Link>
                     <NavLink to={ '/catalogo/coleccion2019' }>Coleccion 2019</NavLink> 
                     <NavLink to={ '/catalogo/coleccion2020' }>Coleccion 2020</NavLink>
                     <NavLink to={ '/catalogo/automaticas' }>Automaticas</NavLink>
--- ecommerce-semas/src/components/cartWidget/cartWidget.jsx
+++ ecommerce-semas/src/components/cartWidget/cartWidget.jsx
@@ -1,9 +1,12 @@
 import Logo from '../../img/carrito.png'
-
+import '../cartWidget/cartWidget.css'
 
 const CartWidget = () => {
     return <>
-    <img className='logo' src={Logo} alt="imagen carrito" title="mi carrito"/>
+    <div className='carritoDeCompras'>
+        <img className='logo' src={Logo} alt="imagen carrito" title="mi carrito"/>
+        <p className=''>0</p>
+    </div>
     </>
 }
 
